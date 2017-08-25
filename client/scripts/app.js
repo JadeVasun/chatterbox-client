9e776551bfb9fe6ef0db2429d65e29dff3e2b494
JadeVasun <jadevasun@gmail.com>
2017-08-25 15:46:19 -0700

    completed up to handleUserNameClick

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,52 @@
 // YOUR CODE HERE:
+var app = {
+  init: function () {
+
+  },
+  send: function (message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function () {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      //url: ,
+      type: 'GET',
+      //data: {},
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  clearMessages: function () {
+    $('#chats').children().remove();
+  },
+  renderMessage: function (message) {
+    $('#chats').append('<div> ' + message.text + ' </div>');
+  },
+  renderRoom: function (value) {
+    $('#roomSelect').append('<div> ' + value + '</div>');
+  },
+  addFriend: function () {
+    
+  }
+
+}
+
